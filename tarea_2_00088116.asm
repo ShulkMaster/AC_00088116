c1c0e82a64c94c05de2fda0d40e816c4aec4dcd0
shulkmaster <shulkmaster@gmail.com>
2020-04-26 23:51:13 -0600

    arreglando ejercicio 2

--- tarea_2_00088116.asm
+++ tarea_2_00088116.asm
@@ -1,8 +1,8 @@
     org 100h
 
 section .develop
-; ejercicio1
-	mov	ax, 0d
+    ; ejercicio1
+    mov	ax, 0d
     add ax, 8d
     add ax, 8d
     add ax, 1d
@@ -10,28 +10,35 @@ section .develop
     add ax, 6d
     mov bl, 5d
     div bl
-	mov	di, 0d
-	mov	cx,len
-	move:
-		mov bh,[msg+di]
-		mov	[di+200h], bh
-		inc di
-		loop move
+    mov	di, 0d
+    mov	cx,len
+    move:
+        mov bh,[msg+di]
+        mov	[di+200h], bh
+        inc di
+        loop move
 
-; ejercicio2
+        ; ejercicio2
     mov	cx, 10d
     mov	ax, 2d
     mov bx, 2d
     mov di, 0d
     duplicate:
         mul bx
+        cmp ax, 255d
+        ja skip
         mov [di+210h], al
         inc di
-        ; if > 255
-        ; mov ax
-        ;inc di x2
-	    loop duplicate
-    
+        loop duplicate
+
+skip:
+    mov [di+210h], ah
+    inc di
+    mov [di+210h], al
+    inc di
+    dec cx
+    jmp duplicate
+
 int 20h
 
 section	.data
