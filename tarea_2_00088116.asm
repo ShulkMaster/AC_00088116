55661337901887c0840d2ff208d4ed1ef0d1c41c
shulkmaster <shulkmaster@gmail.com>
2020-04-26 20:12:41 -0600

    ejercicio 1 de la guia, promedio

--- tarea_2_00088116.asm
+++ tarea_2_00088116.asm
@@ -1,14 +1,11 @@
     org 100h
 section .load
-	mov	di,0d
-	mov	cx,[len]
-	move:
-		mov bh,[msg+di]
-		mov	[di+200h],bh; instead mov [di+ax],bh
-		inc di
-		loop move
+	mov	ax, 0d
+    add ax, 8d
+    add ax, 8d
+    add ax, 1d
+    add ax, 1d
+    add ax, 6d
+    mov bl, 5d
+    div bl
     int 20h
-
-section	.data
-msg	db	"Y sin embargo se mueve"
-len equ $-msg
