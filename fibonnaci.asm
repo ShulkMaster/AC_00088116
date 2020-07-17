7cf589045c5eac7a610d2268bfb218eb609686aa
ShulkMaster <shulkmaster@gmail.com>
2020-07-17 16:56:26 -0600

    agregando codigo para renderizar cuadrados de n*m de ancho

--- fibonnaci.asm
+++ fibonnaci.asm
@@ -1,49 +1,108 @@
-org 100h
+org 200h
 
 section .main
+    global _start
+
+_start:
+    call set_program
     call graphic_mode
-    xor si, si
-    xor di, di
+
+    ; rectangle for 8
+    mov [column], word 246d
+    mov [row], word 0d
+    mov [height], word 480d
+    mov [width], word 394d
     call draw_rectangle
+
+    ; rectangle for 5
+    mov [column], word 0d
+    mov [row], word 180d
+    mov [height], word 300d
+    mov [width], word 247d
+    call draw_rectangle
+
+    ; rectangle for 3
+    mov [column], word 0d
+    mov [row], word 0d
+    mov [height], word 180d
+    mov [width], word 151d
+    call draw_rectangle
+
+    ; rectangle for 2
+    mov [column], word 150d
+    mov [row], word 0d
+    mov [height], word 120d
+    mov [width], word 98d
+    call draw_rectangle
+
+    ; rectangle for 1a
+    mov [column], word 200d
+    mov [row], word 120d
+    mov [height], word 60d
+    mov [width], word 48d
+    call draw_rectangle
+
+    ; rectangle for 1b
+    mov [column], word 150d
+    mov [row], word 120d
+    mov [height], word 60d
+    mov [width], word 50d
+    call draw_rectangle
+
     call keyboard
     int 20h
 
+set_program:
+    xor si, si
+    xor di, di
+    mov [color], byte 0001b
+    ret
+
 graphic_mode:
     mov ah, 00h
+    ; Resolution 640x480
     mov al, 12h
     int 10h
     ret
 
 pixel:
-    mov ah, 0CH
-    mov al, 0111b
+    mov ah,0CH
+    mov al,[color]
     int 10h
     ret
 
-lupi_h:
-    mov cx, 0d ; Columna
-    add cx, si
-    mov	dx, di ; Fila
-    call pixel
-    inc si
-    cmp si, bx
-    jne lupi_h
+draw_line:
+    mov cx,[column]
+    mov di,cx
+    add di,[width]
+    print:
+        call pixel
+        inc cx
+        cmp cx,di
+        jl print
+
     ret
 
 draw_rectangle:
-    mov bx, 60d ;width
-    mov si, 50d
-    mov di, 50d
-
-lupi_rectangle:
-    call lupi_h
-    mov si, 30d
-    inc di
-    cmp di, 80d ;height
-    jb lupi_rectangle
+    mov	dx,[row]
+    mov si,dx
+    add si,[height]
+    stack:
+        call draw_line
+        inc dx
+        cmp dx, si
+        jl stack
+    inc byte [color]
     ret
 
 keyboard:
     mov ah, 00h
     int 16h
     ret
+
+section .data
+column equ 201h
+row equ 203h
+height equ 205h
+width equ 207h
+color equ 209h
